c09fe906f8fd2e7ffca9bc24a6029e7407ed183c
Youhyunwoo <dbgusdn0922@naver.com>
2026-06-12 16:07:25 +0900

    관리자 UID 설정

--- scripts/thread/config.js
+++ scripts/thread/config.js
@@ -6,16 +6,10 @@ export const firebaseConfig = {
     messagingSenderId: "101670166696",
     appId: "1:101670166696:web:7376e468401f223f32e29d",
     measurementId: "G-VQC90L9D74",
-    // apiKey: 'YOUR_API_KEY',
-    // authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    // projectId: 'YOUR_PROJECT_ID',
-    // storageBucket: 'YOUR_PROJECT_ID.firebasestorage.app',
-    // messagingSenderId: 'YOUR_SENDER_ID',
-    // appId: 'YOUR_APP_ID',
 };
 
 export const ADMIN_UIDS = [
-    'YOUR_GOOGLE_ADMIN_UID',
+    'ajK8fhZQPmWIMRHxnAMXFmZEH6i2',
 ];
 
 export function isConfigured() {
